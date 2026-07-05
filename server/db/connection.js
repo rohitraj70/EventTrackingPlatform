623ffc7147623ceb708e72427cb83b2031f915bd
rohitraj70 <rohit.tech0956@gmail.com>
2026-07-05 09:41:42 +0530

    change in connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,11 +1,10 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "ticketing",
-  password: "rohitRaj4577",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
